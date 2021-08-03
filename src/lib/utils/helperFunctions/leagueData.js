55641b745b38bc391a8ec53a93a5d91aaa1a4201
Caseytmessner <88355437+Caseytmessner@users.noreply.github.com>
2021-08-02 20:50:48 -0400

    Update leagueData.js

--- src/lib/utils/helperFunctions/leagueData.js
+++ src/lib/utils/helperFunctions/leagueData.js
@@ -1,8 +1,8 @@
 import { get } from 'svelte/store';
 import {leagueData} from '$lib/stores';
 
-export const leagueID = "650025991288205312"; // your league ID
-export const leagueName = "your_league_name"; // your league name
+export const leagueID = "643134570656501760"; // your league ID
+export const leagueName = "poor_richards_regulars"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 
